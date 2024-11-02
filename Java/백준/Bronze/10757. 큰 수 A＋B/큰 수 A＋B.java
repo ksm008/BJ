87416582ae41f6aefd49a0be787a192d13368f14
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-11-02 15:55:51 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 208 ms, Memory: 17408 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+        BigInteger A = new BigInteger(str[0]);
+        BigInteger B = new BigInteger(str[1]);
+
+        bw.write(A.add(B).toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
