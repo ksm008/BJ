196a8225e5b2e636db6f1f9a76254f66d0a2011e
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-12-04 14:30:29 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 120 ms, Memory: 15936 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        bw.write(":fan::fan::fan:\n");
+        bw.write(":fan::" + str + "::fan:\n");
+        bw.write(":fan::fan::fan:");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
