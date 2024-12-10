aafb67529e470e9e51ba4916181fc66729a8ca06
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-12-11 04:28:04 +0900

    [Bronze V] Title: 자동완성, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24883. 자동완성/자동완성.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        if (str.equals("n") || str.equals("N")) {
+            bw.write("Naver D2");
+        } else {
+            bw.write("Naver Whale");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
