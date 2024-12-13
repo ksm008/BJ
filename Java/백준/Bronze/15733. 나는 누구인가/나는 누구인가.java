8507f31e9cc1e4c7c896a550f8d97357549ccc82
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-12-13 23:40:22 +0900

    [Bronze V] Title: 나는 누구인가, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("I'm Sexy");
+
+        bw.flush();
+        bw.close();
+    }
+}
