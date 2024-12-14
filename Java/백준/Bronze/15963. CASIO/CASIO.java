09302b49d381945f6b5f37d7b629bc298dff89eb
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-12-14 09:06:14 +0900

    [Bronze V] Title: CASIO, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+
+        long A = Long.parseLong(str[0]);
+        long B = Long.parseLong(str[1]);
+
+        if (A == B) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
