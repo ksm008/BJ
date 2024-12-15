a1bce29a6f077f681be7cea7755c3606f287cb2b
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-12-16 02:38:11 +0900

    [Bronze V] Title: 초코바, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] str = br.readLine().split(" ");
+
+        int coins = Integer.parseInt(str[0]) * 100;
+        int choco =Integer.parseInt(str[1]);
+
+        if (coins >= choco) {
+            bw.write("Yes");
+        } else {
+            bw.write("No");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
