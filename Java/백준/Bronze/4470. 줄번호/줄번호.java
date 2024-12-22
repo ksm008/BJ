b45440addfafc8a8f75e3599228a8752aee7d15a
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2024-12-22 11:04:42 +0900

    [Bronze IV] Title: 줄번호, Time: 124 ms, Memory: 16116 KB -BaekjoonHub

---
+++ Java/백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String str = br.readLine();
+
+            bw.write(i + 1 + ". " + str);
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
