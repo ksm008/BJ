8f0d8244a2bdaf1f1332c2cfb646ed0d7326545a
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-01-09 01:39:48 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[][] arrays = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                arrays[i][j] = str.charAt(j);
+            }
+        }
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arrays[j][i] != '\0') {
+                    bw.write(arrays[j][i]);
+                }
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
