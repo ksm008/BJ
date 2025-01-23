102bd724f36d78967d3c815438dca24a86291c6b
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-01-24 06:37:31 +0900

    [Bronze V] Title: 연세대학교, Time: 100 ms, Memory: 14360 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        if (num == 0) {
+            bw.write("YONSEI");
+        } else if (num == 1) {
+            bw.write("Leading the Way to the Future");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
