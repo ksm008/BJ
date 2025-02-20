cb2302deff5100dc5f38af158aac759d4c08ea57
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-02-21 05:52:35 +0900

    [Bronze V] Title: 새로운 시작, Time: 92 ms, Memory: 13868 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15962. 새로운 시작/새로운 시작.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("파이팅!!");
+        bw.flush();
+        bw.close();
+
+    }
+}
