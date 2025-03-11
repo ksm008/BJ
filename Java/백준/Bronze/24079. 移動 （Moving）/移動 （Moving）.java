5782eda9d4118f1325e5a4272440737cf53de94e
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-03-12 00:29:48 +0900

    [Bronze IV] Title: 移動 (Moving), Time: 100 ms, Memory: 14224 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24079. 移動 （Moving）/移動 （Moving）.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+        int Z = Integer.parseInt(br.readLine());
+
+        if (X + Y <= Z) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
