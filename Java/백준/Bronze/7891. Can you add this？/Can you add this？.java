a2d691a6f45e10fa22349bbeba10b126f2c92216
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-03-27 23:25:35 +0900

    [Bronze V] Title: Can you add this?, Time: 116 ms, Memory: 14652 KB -BaekjoonHub

---
+++ Java/백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            String[] str = br.readLine().split(" ");
+
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+
+            bw.write(a + b + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
