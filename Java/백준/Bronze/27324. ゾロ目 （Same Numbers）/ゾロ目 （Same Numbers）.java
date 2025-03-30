be6b3c563dbd10452c99f044950fb90ade74c967
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-03-30 20:45:58 +0900

    [Bronze V] Title: ゾロ目 (Same Numbers), Time: 108 ms, Memory: 14252 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27324. ゾロ目 （Same Numbers）/ゾロ目 （Same Numbers）.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+
+        if (str.charAt(0) == str.charAt(1)) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
