8e80e8937d10997a6f98cf1011b3e29f9116557a
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-04-07 20:40:06 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

---
+++ Java/백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine()) - 1;
+
+        String[][] board = {{"11", "A B C D E F G H J L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A C E F G H I L M"},
+                {"9", "A B C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A C E F G H L M"},
+                {"8", "A B C F G H L M"}};
+
+        bw.write(board[num][0] + "\n");
+        bw.write(board[num][1]);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
