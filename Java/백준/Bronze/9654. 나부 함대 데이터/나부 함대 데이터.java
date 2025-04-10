2f83c610ff9a7a86839f3e6d13f165c4df39e9a3
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-04-11 01:17:22 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 100 ms, Memory: 14332 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(String.format("%-15s%-15s%-11s%-10s\n", "SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE"));
+        bw.write(String.format("%-15s%-15s%-11s%-10s\n", "N2 Bomber", "Heavy Fighter", "Limited", "21"));
+        bw.write(String.format("%-15s%-15s%-11s%-10s\n", "J-Type 327", "Light Combat", "Unlimited", "1"));
+        bw.write(String.format("%-15s%-15s%-11s%-10s\n", "NX Cruiser", "Medium Fighter", "Limited", "18"));
+        bw.write(String.format("%-15s%-15s%-11s%-10s\n", "N1 Starfighter", "Medium Fighter", "Unlimited", "25"));
+        bw.write(String.format("%-15s%-15s%-11s%-10s\n", "Royal Cruiser", "Light Combat", "Limited", "4"));
+
+        bw.flush();
+        bw.close();
+    }
+}
