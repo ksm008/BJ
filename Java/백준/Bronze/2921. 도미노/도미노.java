9dce953d59b4ddbcaeff5b34aefce9011525b72d
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-04-24 17:56:27 +0900

    [Bronze III] Title: 도미노, Time: 116 ms, Memory: 14256 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2921. 도미노/도미노.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine()), cnt = 0, length = num - 1;
+        int result = 0;
+
+        for (int i = 0; i <= num; i++) {
+            for (int j = i;j <= num; j++) {
+                result += i + j;
+            }
+        }
+
+        bw.write(result + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
