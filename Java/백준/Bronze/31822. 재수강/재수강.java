099d086abb135c9b6e42aa60ce24b709c48d4f05
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-05-01 06:30:43 +0900

    [Bronze IV] Title: 재수강, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

---
+++ Java/백준/Bronze/31822. 재수강/재수강.java
@@ -0,0 +1,23 @@
+import java.io.*;  
+  
+public class Main {  
+    public static void main(String[] args) throws IOException {  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+  
+        String subjectCode = br.readLine().substring(0, 5);  
+        int num = Integer.parseInt(br.readLine()), result = 0;  
+  
+        for (int i = 0; i < num; i++) {  
+            if (br.readLine().startsWith(subjectCode)) {  
+                result++;  
+            }  
+        }  
+  
+        bw.write(result + "");  
+  
+        bw.flush();  
+        bw.close();  
+        br.close();  
+    }  
+}
