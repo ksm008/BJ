b321b32714b7324ee701e95d6583a97c859965f5
Seung Min Kwon <169874259+ksm008@users.noreply.github.com>
2025-05-28 13:46:09 +0900

    [Bronze IV] Title: Kuber, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

---
+++ Java/백준/Bronze/20833. Kuber/Kuber.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 1; i <= num; i++) {
+            result += i * i * i;
+        }
+
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
