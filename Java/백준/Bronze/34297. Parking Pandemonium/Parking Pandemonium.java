52f1b00f4d7b45923e2ce4f88e6a1196b3413636
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2025-09-09 09:57:06 +0900

    [Bronze V] Title: Parking Pandemonium, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ Java/백준/Bronze/34297. Parking Pandemonium/Parking Pandemonium.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int M = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        sb.append(M * C);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
