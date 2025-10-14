b2fa7d768d73bf46fc47fbbe6d427be74dbb36bb
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2025-10-14 16:13:00 +0900

    [Gold V] Title: 배, Time: 184 ms, Memory: 16424 KB -BaekjoonHub

--- Java/백준/Gold/1092. 배/배.java
+++ Java/백준/Gold/1092. 배/배.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,42 +9,50 @@ public class Main {
     public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<Integer> crain = new ArrayList<>();
-        List<Integer> boxes = new ArrayList<>();
+        Integer[] crane = new Integer[N];
 
         for (int i = 0; i < N; i++) {
-            crain.add(Integer.parseInt(st.nextToken()));
+            crane[i] = Integer.parseInt(st.nextToken());
         }
 
         int M = Integer.parseInt(br.readLine());
+        Integer[] boxes = new Integer[M];
+
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
-            boxes.add(Integer.parseInt(st.nextToken()));
+            boxes[i] = Integer.parseInt(st.nextToken());
         }
 
-        crain.sort(Collections.reverseOrder());
-        boxes.sort(Collections.reverseOrder());
+        Arrays.sort(crane, Collections.reverseOrder());
+        Arrays.sort(boxes, Collections.reverseOrder());
 
-        int strongestCrain = crain.get(0);
-        int heaviestBox = boxes.get(0);
+        int strongestCrain = crane[0];
+        int heaviestBox = boxes[0];
 
         if (heaviestBox > strongestCrain) {
             sb.append("-1");
         } else {
+            boolean[] checked = new boolean[M];
+            int[] pos = new int[N];
+            int moved = 0;
             int minute = 0;
-            while (!boxes.isEmpty()) {
-                for (int i = 0; i < crain.size(); i++) {
-                    for (int j = 0; j < boxes.size(); j++) {
-                        if (crain.get(i) >=  boxes.get(j)) {
-                            boxes.remove(j);
+
+            while (moved < M) {
+                for (int i = 0; i < N; i++) {
+                    while (pos[i] < M) {
+                        if (!checked[pos[i]] && crane[i] >= boxes[pos[i]]) {
+                            checked[pos[i]] = true;
+                            pos[i]++;
+                            moved++;
                             break;
+                        } else {
+                            pos[i]++;
                         }
                     }
                 }
                 minute++;
             }
-
             sb.append(minute);
         }
 
