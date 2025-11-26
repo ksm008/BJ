6558014e3c38e535e5e5fcd608c99fa0422fabee
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2025-11-27 08:55:29 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 112 ms, Memory: 14372 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,34 @@
+import java.io.*;  
+  
+public class Main {  
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+    static StringBuilder sb = new StringBuilder();  
+  
+    public static void main(String[] args) throws IOException {  
+        int N = Integer.parseInt(br.readLine());  
+  
+        for (int i = 1; i <= N; i++) {  
+            sb.append(" ".repeat(N - i));  
+            for (int j = 0; j < (2 * N - 1); j++) {  
+                if (i != N) {  
+                    if (j < 2 * i - 1) {  
+                        if (j == 0 || j == 2 * i - 2) {  
+                            sb.append("*");  
+                        } else {  
+                            sb.append(" ");  
+                        }  
+                    }  
+                } else {  
+                    sb.append("*");  
+                }  
+            }  
+            sb.append("\n");  
+        }  
+  
+        bw.write(sb.toString());  
+        bw.flush();  
+        bw.close();  
+        br.close();  
+    }  
+}
