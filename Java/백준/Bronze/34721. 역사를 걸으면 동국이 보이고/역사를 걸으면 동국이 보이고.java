a760f393bbf8be4af45b3cdba0aa2b6ea5b47973
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2025-12-15 08:59:16 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ Java/백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -0,0 +1,20 @@
+import java.io.*;  
+  
+public class Main {  
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+    static StringBuilder sb = new StringBuilder();  
+  
+    public static void main(String[] args) throws IOException {  
+        int N = Integer.parseInt(br.readLine());  
+  
+        for (int i = 0; i < N; i++) {  
+            sb.append("I love DGU\n");  
+        }  
+  
+        bw.write(sb.toString());  
+        bw.flush();  
+        bw.close();  
+        br.close();  
+    }  
+}
