215ddfb2c56e61c92bed21a6d4c83961640b6599
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2025-12-28 17:35:53 +0900

    [Bronze IV] Title: 양말 짝 맞추기, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ Java/백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.java
@@ -0,0 +1,22 @@
+import java.io.*;  
+  
+public class Main {  
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+    static StringBuilder sb = new StringBuilder();  
+  
+    public static void main(String[] args) throws IOException {  
+        int result = 0;  
+        for (int i = 0; i < 5; i++) {  
+            int N = Integer.parseInt(br.readLine());  
+  
+            result ^= N;  
+        }  
+  
+        sb.append(result);  
+        bw.write(sb.toString());  
+        bw.flush();  
+        bw.close();  
+        br.close();  
+    }  
+}
