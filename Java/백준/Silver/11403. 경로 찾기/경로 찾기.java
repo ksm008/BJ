0b1b87a010af8b275d512301457164907e8c73e7
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2026-01-19 10:27:25 +0900

    [Silver I] Title: 경로 찾기, Time: 160 ms, Memory: 16800 KB -BaekjoonHub

---
+++ Java/백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        int[][] dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                dist[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (dist[i][k] == 1 && dist[k][j] == 1) {
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sb.append(dist[i][j]).append(" ");
+            }
+            sb.append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
