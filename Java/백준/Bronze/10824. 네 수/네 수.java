9882934b8111382cdc2474cdbc2f937eda674e1f
KwonSeungMin <169874259+ksm008@users.noreply.github.com>
2026-03-12 08:58:53 +0900

    [Bronze III] Title: 네 수, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,22 @@
+import java.io.*;  
+import java.util.StringTokenizer;  
+  
+public class Main {  
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+    static StringBuilder sb = new StringBuilder();  
+  
+    public static void main(String[] args) throws IOException {  
+        StringTokenizer st = new StringTokenizer(br.readLine());  
+  
+        String str1 = st.nextToken() + st.nextToken();  
+        String str2 = st.nextToken() + st.nextToken();  
+  
+        long result = Long.parseLong(str1) + Long.parseLong(str2);  
+        sb.append(result);  
+        bw.write(sb.toString());  
+        bw.flush();  
+        bw.close();  
+        br.close();  
+    }  
+}
